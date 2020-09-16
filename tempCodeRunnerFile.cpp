977d6242ecc92da7ec9f58a77a31dff35721c4b2
panthitesh6410 <panthitesh6410@gmail.com>
2020-09-16 18:22:43 +0530

    nagarro problems

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,20 @@
-+)
-    // {
-    //     for(int j=0;j<n;j++)
-    //     {
-    //         if(bride[i] == groom[j])
-    //     }
-    // }
+// nagarro - mountain climbing :
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int max = arr[0];
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+    cout<<max;
+    return 0;
+}
